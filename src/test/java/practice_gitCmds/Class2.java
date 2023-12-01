6607e895740af944e49e265963021c557d424de7
prasanthGitpractice <galiprasanth6@gmail.com>
2023-12-01 21:15:47 +0530

    c1-c3

--- src/test/java/practice_gitCmds/Class2.java
+++ src/test/java/practice_gitCmds/Class2.java
@@ -5,6 +5,10 @@ public class Class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("commit3");
+		System.out.println("commit c1");
+		System.out.println("commit c2");
+		System.out.println("commit c3");
+
 	}
 
 }
